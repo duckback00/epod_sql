0d7340e73d8c5011fa1512c616920035e1e0a822
Alan Bitterman <duckback00@gmail.com>
2020-09-21 10:40:01 -0400

    Update notes.sql

--- sql_code/sql_direct/notes.sql
+++ sql_code/sql_direct/notes.sql
@@ -1,4 +1,5 @@
 create table notes (id number(38), note varchar(200) );
 insert into notes values (1,'Hello World');
 insert into notes values (2,'Hey There!');
+insert into notes values (3,'More notes');
 commit;
