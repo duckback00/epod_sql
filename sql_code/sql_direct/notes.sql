2bc19d9893fbc218a7a7ad9e50e51ec2b645b297
Alan Bitterman <duckback00@gmail.com>
2020-09-21 10:50:49 -0400

    Update notes.sql

--- sql_code/sql_direct/notes.sql
+++ sql_code/sql_direct/notes.sql
@@ -6,6 +6,8 @@ EXCEPTION
          RAISE;
       END IF;
 END;
+/
+
 create table notes (id number(38), note varchar(200) );
 insert into notes values (1,'Hello World');
 insert into notes values (2,'Hey There!');
