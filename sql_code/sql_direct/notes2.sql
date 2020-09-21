905e006d73456d3511454f408dff6f731919f07c
Alan Bitterman <duckback00@gmail.com>
2020-09-21 11:10:28 -0400

    Add files via upload

---
+++ sql_code/sql_direct/notes2.sql
@@ -0,0 +1,16 @@
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE NOTES';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
+
+create table notes (id number(38), note varchar(200) );
+insert into notes values (1,'Hello World');
+insert into notes values (2,'Hey There!');
+insert into notes values (3,'More notes');
+commit;
+
