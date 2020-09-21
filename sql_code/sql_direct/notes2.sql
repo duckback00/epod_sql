9144f800f3ee05703a0a34be5727b7f15c430cfe
Alan Bitterman <duckback00@gmail.com>
2020-09-21 15:54:52 -0400

    Update notes2.sql

--- sql_code/sql_direct/notes2.sql
+++ sql_code/sql_direct/notes2.sql
@@ -22,8 +22,8 @@ BEGIN
   Select count(*) into v_column_exists
     from user_tab_cols
     where upper(column_name) = 'STATE'
-      and upper(table_name) = 'EMPLOYEES'
-      and owner = 'SCOTT';   
+      and upper(table_name) = 'EMPLOYEES';
+      --and owner = 'DELPHIXDB';  -- if using all_xxx or dba_xxx   
   if (v_column_exists = 0) then
       execute immediate 'alter table employees add (state varchar2(40))';
   end if;
